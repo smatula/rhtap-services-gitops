08ce799eebefe77ff5c629055ef5d11ac1bfa31d
Stephen Matula <smatula@redhat.com>
2025-08-19 11:54:18 -0400

    artifactory testing

--- create_secrets.sh
+++ create_secrets.sh
@@ -2,15 +2,19 @@
 
 source ./envfile
 
-# Create namespaces where the secret will be
-oc new-project hive
+# # Create namespaces where the secret will be
+# oc new-project hive
 
 
-# Create secrets
-echo "Creating Pull Secret"
-oc create secret generic global-pull-secret --type=kubernetes.io/dockerconfigjson --from-literal=.dockerconfigjson="$PULL_SECRET" -n hive
+# # Create secrets
+# echo "Creating Pull Secret"
+# oc create secret generic global-pull-secret --type=kubernetes.io/dockerconfigjson --from-literal=.dockerconfigjson="$PULL_SECRET" -n hive
 
-echo "Creating AWS creds secret"
-oc create secret generic hive-aws-creds -n hive --from-literal=aws_access_key_id="$AWS_ACCESS_KEY_ID" --from-literal=aws_secret_access_key="$AWS_SECRET_ACCESS_KEY"
+# echo "Creating AWS creds secret"
+# oc create secret generic hive-aws-creds -n hive --from-literal=aws_access_key_id="$AWS_ACCESS_KEY_ID" --from-literal=aws_secret_access_key="$AWS_SECRET_ACCESS_KEY"
 
+
+oc new-project artifactory
+
+oc create secret generic artifactory-admin-creds -n artifactory --from-literal=new-admin-password=admin123
 
