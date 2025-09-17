fd265d734c3337e78c362c61448a95bbd627bee2
Stephen Matula <smatula@redhat.com>
2025-09-17 08:33:34 -0400

    Test nexus config

--- create_secrets.sh
+++ create_secrets.sh
@@ -3,13 +3,14 @@
 source ./envfile
 
 # Create namespaces where the secret will be
-oc new-project hive
-oc new-project artifactory
+#oc new-project hive
+#oc new-project artifactory
+oc new-project nexus
 
 # Create secrets
-echo "Creating Pull Secret"
-oc create secret generic global-pull-secret --type=kubernetes.io/dockerconfigjson --from-literal=.dockerconfigjson="$PULL_SECRET" -n hive
+#echo "Creating Pull Secret"
+#oc create secret generic global-pull-secret --type=kubernetes.io/dockerconfigjson --from-literal=.dockerconfigjson="$PULL_SECRET" -n hive
 
-echo "Creating AWS creds secret"
-oc create secret generic hive-aws-creds -n hive --from-literal=aws_access_key_id="$AWS_ACCESS_KEY_ID" --from-literal=aws_secret_access_key="$AWS_SECRET_ACCESS_KEY"
+#echo "Creating AWS creds secret"
+#oc create secret generic hive-aws-creds -n hive --from-literal=aws_access_key_id="$AWS_ACCESS_KEY_ID" --from-literal=aws_secret_access_key="$AWS_SECRET_ACCESS_KEY"
 
