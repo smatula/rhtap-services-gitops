86222835c1d0ded2531b84b3444b9c4463cd978b
Stephen Matula <smatula@redhat.com>
2025-11-17 10:12:59 -0500

    Update TPA to use application set and cluster generator

---
+++ components/register-cluster/register_cluster.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -euo pipefail
+
+# Get cluster ingress domain
+INGRESS_DOMAIN=$(oc -n openshift-ingress-operator get ingresscontrollers.operator.openshift.io default -o jsonpath='{.status.domain}' 2>/dev/null || echo "apps.example.com")
+
+# Create Argo manager cluster SA
+kubectl create serviceaccount cluster-argocd-manager -n kube-system --dry-run=client -o yaml | kubectl apply -f -
+kubectl create clusterrolebinding cluster-argocd-manager-binding --clusterrole=admin --serviceaccount=kube-system:cluster-argocd-manager --dry-run=client -o yaml | kubectl apply -f -
+kubectl apply -f components/register-cluster/cluster_sa_token.yaml
+
+# Set Server variables
+export SERVER_URL=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}' 2>/dev/null)
+export CA_DATA=$(kubectl get cm kube-root-ca.crt -o jsonpath="{['data']['ca\\.crt']}" | base64 -w 0)
+export BTOKEN=$(kubectl get secret cluster-argocd-manager-token -n kube-system -o jsonpath="{.data.token}" | base64 --decode)
+
+# Set namespace variables
+TPA_NAMESPACE="tssc-tpa"
+REALM="chicken"
+
+# Derive annotation values
+export APP_DOMAIN_URL="-${TPA_NAMESPACE}.${INGRESS_DOMAIN}"
+export KEYCLOAK_HOST="sso.${INGRESS_DOMAIN}"
+export OIDC_ISSUER_URL="https://sso.${INGRESS_DOMAIN}/realms/${REALM}"
+export REDIRECT_URI1=https://server${APP_DOMAIN_URL}
+export REDIRECT_URI2=https://server${APP_DOMAIN_URL}/*
+export REDIRECT_URI3=https://sbom${APP_DOMAIN_URL}
+export REDIRECT_URI4=https://sbom${APP_DOMAIN_URL}/*
+
+# Add Cluster secret to register
+envsubst < components/register-cluster/cluster_secret.yaml | kubectl apply -f -
